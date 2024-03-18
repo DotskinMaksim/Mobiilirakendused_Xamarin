59a00d9f68c82eedbb0742c33b2607ac2f0f71fb
Maksim <124581027+MaksimDotskin@users.noreply.github.com>
2024-03-18 13:30:39 +0200

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mobileAppDotskin
+{
+    internal class Player
+    {
+
+        public string sym;
+        public bool isBot;
+        public Player(string Sym, bool IsBoot) { 
+
+
+            sym= Sym;
+            isBot= IsBoot;    
+        }
+    }
+}
